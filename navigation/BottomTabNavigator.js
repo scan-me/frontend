16a0ac751f25ae7c9612ab7aa69df0c949b03bc6
Minard-NG <michael4sure20@gmail.com>
2024-07-04 08:23:36 +0200

    updated the home screen

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,20 +1,40 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import HomeScreen from '../screens/HomeScreen';
-import ScanScreen from '../screens/ScanScreen';
-import ResultScreen from '../screens/ResultScreen';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import HomeScreen from "../screens/HomeScreen";
+import ScanScreen from "../screens/ScanScreen";
+import ResultScreen from "../screens/ResultScreen";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name='Home' component={HomeScreen} />
-            <Tab.Screen name='Scan' component={ScanScreen} />
-            <Tab.Screen name="Results" component={ResultScreen} />
-        </Tab.Navigator>
-    );
-}
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name == "Scan") {
+            iconName = focused ? "camera" : "camera-outline";
+          } else if (route.name === "Results") {
+            iconName = focused ? "list" : "list-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "green",
+        inactiveTIntColor: "gray",
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Scan" component={ScanScreen} />
+      <Tab.Screen name="Results" component={ResultScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default BottomTabNavigator;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,3 +1,26 @@
-function HomeScreen() {}
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Scan Me for Pigs</Text>
+    </View>
+  );
+}
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#2e7d32",
+  },
+});
